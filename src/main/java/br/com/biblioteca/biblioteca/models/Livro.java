f1d79cf71f784c94b9a29ff5de9544b0ce89b81e
Fabizzz <165857418+Fabiz2@users.noreply.github.com>
2026-04-27 15:54:38 -0300

    criação models + conexao com banco de dados

--- src/main/java/br/com/biblioteca/biblioteca/models/Livro.java
+++ src/main/java/br/com/biblioteca/biblioteca/models/Livro.java
@@ -1,4 +1,59 @@
 package br.com.biblioteca.biblioteca.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="livros")
 public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String nome;
+    @Column(nullable = false, length = 100  )
+    private String autor;
+    @Column(nullable = false, length = 100)
+    private String genero;
+
+    public Livro(Long id, String nome, String autor, String genero) {
+        this.id = id;
+        this.nome = nome;
+        this.autor = autor;
+        this.genero = genero;
+    }
+
+    public Livro(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 }
