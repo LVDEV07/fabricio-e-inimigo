996696df2b4b36891fbe123391038e00611e4688
Fabizzz <165857418+Fabiz2@users.noreply.github.com>
2026-04-29 16:33:24 -0300

    Criação do controller e cadastro dos livros

---
+++ src/main/java/br/com/biblioteca/biblioteca/repository/LivroRepository.java
@@ -0,0 +1,15 @@
+package br.com.biblioteca.biblioteca.repository;
+
+import br.com.biblioteca.biblioteca.models.Livro;
+import br.com.biblioteca.biblioteca.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    Optional<Livro> findByNome(String nome);
+
+}
