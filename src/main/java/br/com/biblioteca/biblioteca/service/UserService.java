3e47c239ba84255fea92f6147c3b9e8121542e8c
Fabizzz <165857418+Fabiz2@users.noreply.github.com>
2026-05-13 09:16:01 -0300

    Configuração do SecurityConfig e UserService

---
+++ src/main/java/br/com/biblioteca/biblioteca/service/UserService.java
@@ -0,0 +1,45 @@
+package br.com.biblioteca.biblioteca.service;
+
+
+import br.com.biblioteca.biblioteca.Enums.Cargo;
+import br.com.biblioteca.biblioteca.models.User;
+import br.com.biblioteca.biblioteca.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Credencias invalidas"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getSenha())
+                .roles(user.getCargo().toString())
+                .build();
+    }
+
+    public void register(String username, String password, Cargo cargo){
+        User user = new User();
+        user.setEmail(username);
+        user.setCargo(cargo);
+        user.setSenha(passwordEncoder.encode(password));
+
+        userRepository.save(user);
+    }
+
+
+}
