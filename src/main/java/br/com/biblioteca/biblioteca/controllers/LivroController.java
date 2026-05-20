49853ed0ee155fbbaac6af8bf488eb6905b372ee
Lucas <lucas.fst.dev@gmail.com>
2026-05-20 16:45:55 -0300

    Correção do bud de senha e validação se o usuário ultrapassou o prazo de entrega

--- src/main/java/br/com/biblioteca/biblioteca/controllers/LivroController.java
+++ src/main/java/br/com/biblioteca/biblioteca/controllers/LivroController.java
@@ -124,7 +124,7 @@ public class LivroController {
         }
 
         user.getLivrosAlugados().add(idLivro);
-        user.setDataUltimoAluguel(LocalDateTime.now());
+        user.getDatasAluguel().put(idLivro, LocalDateTime.now());
         userRepository.save(user);
 
         redirectAttributes.addFlashAttribute("mensagemSucesso", "Livro alugado com sucesso!");
@@ -140,12 +140,8 @@ public class LivroController {
 
         List<Livro> livros = livroRepository.findAllById(user.getLivrosAlugados());
 
-        if(user.getDataUltimoAluguel() != null){
-            model.addAttribute("dataDevolucao", user.getDataUltimoAluguel().plusDays(7));
-        }
-
+        model.addAttribute("datasAluguel", user.getDatasAluguel());
         model.addAttribute("livros", livros);
-        model.addAttribute("dataDevolucao", user.getDataUltimoAluguel().plusDays(7));
         return "MeusAlugados";
     }
 
